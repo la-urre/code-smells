f532ca019fcb0d6dadae2cd8f3c919a1bd981b78
Damien Urruty <damien.urruty@legrand.fr>
2020-01-21 13:55:16 +0100

    Migrate to junit jupiter. Add dependency to AssertJ.

--- assignment/src/test/java/nerdschool/bar/PubPricesTest.java
+++ assignment/src/test/java/nerdschool/bar/PubPricesTest.java
@@ -1,4 +1,13 @@
 package nerdschool.bar;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PubPricesTest {
+    @Test
+    public void it() {
+        
+    }
+
 }
